c14d8761615860430e0ccdb5a3e93daf60a06afd
KwanjungKim <kwanjungkim@naver.com>
2022-02-08 14:37:01 +0900

    Daily practice first upload

---
+++ 20220124-work/debugging/hello.js
@@ -0,0 +1,4 @@
+const hello = "hello";
+const numbers = [1,2,3,4,5,6];
+console.log(hello);
+console.log(numbers)
---
+++ 20220124-work/test/test.js
@@ -0,0 +1,4 @@
+const student = ['첫번째', '두번째', '세번째'];
+//콘솔로 찍어보자
+student.pop(student[0]);
+console.log(student);
